63b6845a0b165ed607aea9264eccaa473b2780e6
MathFabr <44392793+MathFabr@users.noreply.github.com>
2018-10-23 12:56:21 +0200

    Update scripts/1d-mol.py

--- scripts/1d-mol.py
+++ scripts/1d-mol.py
@@ -3,29 +3,39 @@
 
 import os
 import re
+import signal
+import sys
 
 from random import randrange
-r = randrange(1,100)
+
+
+random_number = randrange(1,100)
 pattern = re.compile('^[0-9]*$')
 nbr = 0
 sortie = "q"
 
+def youcant(sig, frame):
+    print("Aurevoir, merci d'avoir jouer, la réponse était: " + str(random_number))
+    sys.exit(0)
 
- 
-while (nbr != r or nbr !=sortie) :
+
+signal.signal(signal.SIGINT, youcant)
+
+
+while (nbr != random_number or nbr !=sortie) :
   nbr = input("Entrer une valeurs entre 0 et 100: ")
-
+  
   if (nbr == sortie):
-      r = (str(r))
+      random_number = (str(random_number))
       print("Merci d'avoir tenté votre chance, la réponse était: " + r)
 
   else:
       if pattern.match(nbr):
         nbr=(int(nbr))
 
-        if (nbr<r):
+        if (nbr<random_number):
           print("Le nombre à trouver est plus grand")
-        elif (nbr>r):
+        elif (nbr>random_number):
             print("Le nombre à trouver est plus petit")
         else:
             print("Vous avez trouver la réponse!!")
