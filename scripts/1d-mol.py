fb721848f3abceb7bad7f25f393a7bf0e1bdda4f
MathFabr <44392793+MathFabr@users.noreply.github.com>
2018-10-23 12:00:38 +0200

    Update scripts/1d-mol.py

--- scripts/1d-mol.py
+++ scripts/1d-mol.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3.6
 
 
-import random
+import os
 import re
 
-
-r=random.randint(1,100)
+from random import randrange
+r = randrange(1,100)
 pattern = re.compile('^[0-9]*$')
 nbr = 0
 
 
  
 while (nbr != r) :
-
   nbr = input("Entrer une valeurs entre 0 et 100: ")
-
   if pattern.match(nbr):
-
     nbr=(int(nbr))
-
   else:
       print("erreur, vous n'avez pas rentré des chiffres")
-      if (nbr<r):
+  if (nbr<r):
           print("Le nombre à trouver est plus grand")
-      elif (nbr>r):
-            print("Le nombre à trouver est plus grands")
-      else:
+  elif (nbr>r):
+            print("Le nombre à trouver est plus petit")
+  else:
             print("Vous avez trouver la réponse!!")
 
 
