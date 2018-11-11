396e96612b413e9129228a458bf2b3c79c2b451e
MathFabr <44392793+MathFabr@users.noreply.github.com>
2018-11-11 17:58:30 +0100

    Modification final

--- scripts/2a/2a.py
+++ scripts/2a/2a.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3.6
 
-################################################################################################
 
 import os
 import re
 import signal
 import sys
 
-from time import sleep
 from random import randrange
+from time import sleep
 
-###############################################################################################
+##########################################################################################
 
 random_number = randrange(1,100)
 pattern = re.compile('^[0-9]*$')
@@ -18,57 +17,60 @@ nbr = 0
 sortie = "q"
 old_nbr = "a"
 
-###############################################################################################
+#########################################################################################
 
-def youcant(sig, frame):
-    writeToFile("Aurevoir, merci d avoir jouer, la reponse etait: " + str(random_number))
+def youcant(sig, frame):#Message a afficher en "ctrl+C"
+    print("Aurevoir, merci d'avoir jouer, la réponse était: " + str(random_number))
     sys.exit(0)
 
 
-def writeToFile(message):
-      write = open("./jeu.txt", "w")
-      write.write(message)
-      write.close()
+def writeToFile (message): #Ecrire dans le fichier
+    f = open("jeu.txt","w")
+    f.write(message)
+    f.close()
 
+def readToFile ():#Lire le fichier
+    f=open("jeu.txt","r")
+    proposition = f.read()
+    f.close()
+    return proposition
 
-def readToFile():
-      f = open("./jeu.txt", "r")
-      proposition = f.read()
-      f.close()
-      return proposition
-
-###############################################################################################
+#############################################################################################
 
 signal.signal(signal.SIGINT, youcant)
 
-writeToFile("Bonjour et bienvenu dans le jeu du plus ou moins, vous devez trouver une valeurs entre 0 et 100, allez y, proposer. \n (Effacer la ligne et ecriver juste votre nombre a la place)") 
+print("Pour commencer à jouer rdv dans le fichier jeu.txt")
 
-while (nbr != random_number or nbr != sortie) :
- 
+writeToFile("Bonjour, bienvenue dans le jeu du plus ou moins, pour commencer effacer le contenue du fichier afin d entrer votre proposition \n Le nombre a trouver se situe entre 0 et 100")
+
+sleep(10)
+
+nbr = old_nbr
+
+while (nbr != random_number or nbr !=sortie) :
   nbr = readToFile()
-  print(nbr)
+  
 
-  if nbr == old_nbr:
-        continue
-  else:      
+  if nbr == old_nbr :
+     sleep(1)   
+     continue
 
-      old_nbr = nbr
+  nbr = old_nbr
 
-      if (nbr == sortie):#si on mets un "q"
-            random_number = (str(random_number))
-            
-            writeToFile("Merci d avoir tente votre chance, la reponse etait: " + random_number)
-            
-            sys.exit(0)
+  if (nbr == sortie):
+      random_number = (str(random_number))
+      writeToFile("Merci d'avoir jouer, la reponse etait: "+ random_number)
+      sys.exit(0)
 
-      elif pattern.match(nbr):#si on as bien entré un nombre
-            nbr=(int(nbr))
-            if (nbr<random_number):
-                        print("Le nombre à trouver est plus grand")
-            elif (nbr>random_number):
-                        print("Le nombre à trouver est plus petit")
-            else:
-                  print("Vous avez trouver la réponse!!")
-                  sys.exit(0)
-      else:
-                  print("erreur, vous n'avez pas rentré des chiffres")
+  
+  if pattern.match(nbr):
+     nbr=(int(nbr))
+     if (nbr<random_number):
+       writeToFile("Le nombre à trouver ets plus grand")
+     elif (nbr>random_number):
+       writeToFile("Le nombre à trouver est plus petit")
+     else:
+       writeToFile("Vous avez trouver la bonne réponse")
+       sys.exit(0)
+  else:
+       writeToFile("erreur, vous n'avez pas rentré des chiffres")
